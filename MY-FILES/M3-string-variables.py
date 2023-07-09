eddf48a8f74dc5a47086a5b46003d1bee82aa037
JandaSystems <122146268+JandaSystems@users.noreply.github.com>
2023-07-09 11:29:51 +0100

    configure .json for VSC external terminal
    attempts to make changes to .json for VSC external terminal. did not work out so now trying to run .py files in powershell

---
+++ MY-FILES/M3-string-variables.py
@@ -0,0 +1,9 @@
+# x = input("How much did you invest? ")
+# print(x)
+# ``this will get input from the user and save it in the variable [x], and print to screen``
+
+# y = "Bitcoin"
+#``this will declare the value of the variable [y]``
+
+#rules: no spaces, case sensitive, cannot start with a number, stick to one convention
+
