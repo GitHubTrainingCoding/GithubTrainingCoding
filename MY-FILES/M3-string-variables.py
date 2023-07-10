c1f8cb3bdff0773cc8399cf79b5ac515e757064e
JandaSystems <122146268+JandaSystems@users.noreply.github.com>
2023-07-10 10:43:01 +0100

    Update M3-string-variables.py
    updated initial notes section prior to writing code

--- MY-FILES/M3-string-variables.py
+++ MY-FILES/M3-string-variables.py
@@ -1,3 +1,5 @@
+# INITIALIZE USER VARIABLES FIRST: Declare a value for them before they're [input]
+
 # x = input("How much did you invest? ")
 # print(x)
 # ``this will get input from the user and save it in the variable [x], and print to screen``
@@ -6,4 +8,16 @@
 #``this will declare the value of the variable [y]``
 
 #rules: no spaces, case sensitive, cannot start with a number, stick to one convention
+
+# print("Hello " + firstName + " " + lastName) 
+#`` this prints a string, then a variable, then a space, then a variable
+
+# message = 'Hello world' 
+# print(message.lower()) 
+# print(message.upper()) 
+# print(message.swapcase())
+#`` STRING FUNCTIONS to modify case of user's input to variable [message]
+
+#challenge: write a program that allows a user to modify a story.
+
 
